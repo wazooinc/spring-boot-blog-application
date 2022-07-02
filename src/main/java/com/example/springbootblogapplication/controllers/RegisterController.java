c0dfbbc72c651e856535f755e571891c0ac38528
Erik Yuzwa <erikyuzwa@gmail.com>
2022-07-02 11:30:39 -0600

    add basic login and register support

---
+++ src/main/java/com/example/springbootblogapplication/controllers/RegisterController.java
@@ -0,0 +1,34 @@
+package com.example.springbootblogapplication.controllers;
+
+import com.example.springbootblogapplication.models.Account;
+import com.example.springbootblogapplication.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegisterController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping("/register")
+    public String getRegisterForm(Model model) {
+
+        Account account = new Account();
+        model.addAttribute("account", account);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewUser(@ModelAttribute Account account) {
+
+        accountService.save(account);
+
+        return "redirect:/";
+    }
+
+}
