19f560f18409d0c8e5c9eb59d20356f7e22f7f8b
Erik Yuzwa <erikyuzwa@gmail.com>
2022-07-02 09:50:44 -0600

    initial commit and server port update

---
+++ src/test/java/com/example/springbootblogapplication/SpringBootBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootblogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
