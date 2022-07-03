33f1b05a3038ed2780f70c0193ba57e203f400a3
Erik Yuzwa <erikyuzwa@gmail.com>
2022-07-03 09:39:18 -0600

    initial spring security config

---
+++ src/main/java/com/example/springbootblogapplication/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.springbootblogapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login", "/register").permitAll()
+                .antMatchers(HttpMethod.GET, "/posts/*").permitAll()
+                .anyRequest().authenticated();
+    }
+
+}
